178d14bc25e631e7954d8a890554adfe3bab3f1c
nonx0110 <19s005oftn@gmail.com>
2020-09-02 12:23:15 +0900

    up

--- migrations/20200902113209_CreateThread.php
+++ migrations/20200902113209_CreateThread.php
@@ -2,14 +2,26 @@
 
 use Phpmig\Migration\Migration;
 
-class CreateThread extends Migration
+class CreateTodo extends Migration
 {
     /**
      * Do the migration
      */
     public function up()
     {
-
+        $sql = <<<EOT
+CREATE TABLE IF NOT EXISTS `todo` (
+`id` int(11) AUTO_INCREMENT,
+`title` varchar(255) NOT NULL,
+`due_date` DATE NOT NULL,
+`status` tinyint(1) NOT NULL DEFAULT 0,
+`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`deleted_at` DATETIME DEFAULT NULL,
+PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4;
+EOT;
+        $container = $this->getContainer();
+        $container['db']->query($sql);
     }
 
     /**
@@ -17,6 +29,8 @@ class CreateThread extends Migration
      */
     public function down()
     {
-
+        $sql = "DROP TABLE `todo`;";
+        $container = $this->getContainer();
+        $container['db']->query($sql);
     }
 }
