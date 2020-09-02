7db740dc670e8cbafcc80fe28c14dcda734ca8fd
nonx0110 <19s005oftn@gmail.com>
2020-09-02 10:27:46 +0900

    new

--- index.php
+++ index.php
@@ -1,38 +1,12 @@
-<?php
-session_start();
-
-function setToken()
-{
-    $token = sha1(uniqid(mt_rand(), true));
-    $_SESSION['token'] = $token;
-}
-
-function checkToken()
-{
-    if (empty($_SESSION['token'])) {
-        echo "Sessionが空です";
-        exit;
-    }
-
-    if (($_SESSION['token']) !== $_POST['token']) {
-        echo "不正な投稿です。";
-        exit;
-    }
-
-    $_SESSION['token'] = null;
-}
-
-if (empty($_SESSION['token'])) {
-    setToken();
-}
-?>
-
+<!DOCTYPE>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
     <title>TODO App</title>
 </head>
 <body>
@@ -41,26 +15,31 @@ if (empty($_SESSION['token'])) {
     <div class="col-md-8">
         <h1 class="text-center text-primary py-3">TODO App</h1>
 
-        <h2 class="text-muted py-3">TODO作成 </h2>
-        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-            <h6 class="task_name">タスク名</h6>
-            <input type="text" class="form-control" name="personal_name" placeholder="タスク名" required><br>
-            <h6 class="kigen">期限</h6>
-            <!-- 日付入力 -->
-            <input type="date"></input>
-            <br><br><br><br>
-            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
+        <h2 class="text-muted py-3">TODO作成</h2>
+        <form method="POST" action="/index.php">
+            <div class="form-group">
+                <label for="title">タスク名</label>
+                <input type="text" class="form-control" name="title" id="title" placeholder="タスク名" required>
+            </div>
+            <div class="form-group">
+                <label for="due_date">期限</label>
+                <input type="text" class="form-control" name="due_date" id="due_date" required>
+            </div>
+            <br><br>
+            <input type="hidden" name="token" value="ff77bf1a4e1365c589568883c665a2f2f7ab48f9">
             <input class="btn btn-primary"  type="submit" name="btn" value="TODOを作成する">
         </form>
 
-        <h3 class="text-muted py-3">やること一覧 </h3>
+        <hr>
 
-        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+        <h2 class="text-muted py-3">やること一覧</h2>
+
+        <form method="POST" action="/index.php">
             <input type="hidden" name="method" value="DELETE">
             <button class="btn btn-danger" type="submit">TODOを全削除する</button>
         </form>
-
-        <thead>
+                <table class="table">
+            <thead>
             <tr>
                 <th>タイトル</th>
                 <th>状態</th>
@@ -68,34 +47,82 @@ if (empty($_SESSION['token'])) {
                 <th>更新</th>
             </tr>
             </thead>
-<?php
-
-date_default_timezone_set('Asia/Tokyo');
-const TODO_FILE = 'todo.txt';
-
-require_once './todo.php';
-$todo = new TODO('TODO App');
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
-        $todo->delete();
-    } else {
-        $todo->post($_POST['personal_name'], $_POST['contents']);
-    }
-
-    // ブラウザのリロード対策
-    $redirect_url = $_SERVER['HTTP_REFERER'];
-    header("Location: $redirect_url");
-    exit;
-}
-
-echo $todo->getList();
-
-?>
+            <tbody>
+                        <tr>
+                <form method="POST" action="/index.php">
+                    <td>ごみだし</td>
+                    <td>2020-07-15</td>
+                    <td class="label">
+                        <label>
+                            <select name="status" class="form-control">
+                                <option value='0' >未着手</option><option value='1' selected>作業中</option><option value='2' >完了</option>                            </select>
+                        </label>
+                    </td>
+                    <td>
+                        <input type="hidden" name="method" value="UPDATE">
+                        <input type="hidden" name="todo_id" value="3">
+                        <button class="btn btn-primary" type="submit">変更</button>
+                    </td>
+                </form>
+            </tr>
+                        <tr>
+                <form method="POST" action="/index.php">
+                    <td>メール送る</td>
+                    <td>2020-07-23</td>
+                    <td class="label">
+                        <label>
+                            <select name="status" class="form-control">
+                                <option value='0' >未着手</option><option value='1' >作業中</option><option value='2' selected>完了</option>                            </select>
+                        </label>
+                    </td>
+                    <td>
+                        <input type="hidden" name="method" value="UPDATE">
+                        <input type="hidden" name="todo_id" value="5">
+                        <button class="btn btn-primary" type="submit">変更</button>
+                    </td>
+                </form>
+            </tr>
+                        <tr>
+                <form method="POST" action="/index.php">
+                    <td>宿題</td>
+                    <td>2020-07-30</td>
+                    <td class="label">
+                        <label>
+                            <select name="status" class="form-control">
+                                <option value='0' selected>未着手</option><option value='1' >作業中</option><option value='2' >完了</option>                            </select>
+                        </label>
+                    </td>
+                    <td>
+                        <input type="hidden" name="method" value="UPDATE">
+                        <input type="hidden" name="todo_id" value="4">
+                        <button class="btn btn-primary" type="submit">変更</button>
+                    </td>
+                </form>
+            </tr>
+                        </tbody>
+        </table>
+    </div>
+</div>
 
 <!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</script>
+<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
+<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
+</script>
+<script>
+  flatpickr(document.getElementById('due_date'), {
+    locale: 'ja',
+    dateFormat: "Y/m/d",
+    minDate: new Date()
+  });
+</script>
 </body>
 </html>
--- todo.php
+++ todo.php
@@ -1,47 +1,19 @@
 <?php
 
-class TODO
+class Todo
 {
-    private $name;
-    private const TODO_FILE = 'todo.txt';
-    private $dbh;
-
-    public function __construct(string $name)
+    // コンストラクタ
+    public function __construct()
     {
-        $this->name = $name;
-        $this->dbh = new PDO('mysql:dbname=todo;host=127.0.0.1', 'root', 'password');
     }
 
-    public function getList()
-    {
-        $res = "";
-        $stmt = $this->dbh->query("SELECT * FROM `todo` WHERE `deleted_at` IS NULL ORDER BY `created_at` ASC");
-        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-            $created_at = $row["created_at"];
-            $name = $row["name"];
-            $contents = $row["content"];
-            $res .= "<hr>\n";
-            $res .= "<p>投稿日時: ".date("Y/m/d H:i:s", strtotime($created_at))."</p>\n";
-            $res .= "<p>投稿者:".$name."</p>\n";
-            $res .= "<p>内容:</p>\n";
-            $res .= "<p>".nl2br($contents)."</p>\n";
-        }
 
-        return $res;
+    /**
+     * @param string $title
+     * @param string $due_date
+     */
+    public function post(string $title, string $due_date)
+    {
     }
 
-    public function post(string $personal_name, string $contents) 
-    {
-        $stmt = $this->dbh->prepare("INSERT INTO `todo` (name, content) VALUE (:name, :content)");
-        $stmt->bindParam(':name', $personal_name, PDO::PARAM_STR);
-        $stmt->bindParam(':content', $contents, PDO::PARAM_STR);
-        $stmt->execute();
-    }
-
-    public function delete() 
-    {
-        $sql = "UPDATE `todo` SET `deleted_at` = NOW()";
-        $stmt = $this->dbh->prepare($sql);
-        $stmt->execute();
-    }
 }
